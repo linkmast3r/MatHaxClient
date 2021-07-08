05875061866f1f85ed2fbd518ad8b9075dfd0d14
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2021-07-08 09:00:36 +0200

    Removed not working color codes from Server Brand HUD Module

--- src/main/java/matejko06/mathax/systems/modules/render/hud/modules/ServerBrandHud.java
+++ src/main/java/matejko06/mathax/systems/modules/render/hud/modules/ServerBrandHud.java
@@ -12,7 +12,7 @@ public class ServerBrandHud extends TripleTextHudElement {
     protected String getRight() {
         if (!Utils.canUpdate()) return "None";
 
-        return mc.player.getServerBrand();
+        return mc.player.getServerBrand().replace("§a", "").replace("§b", "").replace("§c", "").replace("§d", "").replace("§e", "").replace("§1", "").replace("§2", "").replace("§3", "").replace("§4", "").replace("§5", "").replace("§6", "").replace("§7", "").replace("§8", "").replace("§9", "").replace("§k", "").replace("§l", "").replace("§m", "").replace("§n", "").replace("§o", "").replace("§r", "").replace("&a", "").replace("&b", "").replace("&c", "").replace("&d", "").replace("&e", "").replace("&1", "").replace("&2", "").replace("&3", "").replace("&4", "").replace("&5", "").replace("&6", "").replace("&7", "").replace("&8", "").replace("&9", "").replace("&k", "").replace("&l", "").replace("&m", "").replace("&n", "").replace("&o", "").replace("&r", "");
     }
 
     public String getEnd() {
